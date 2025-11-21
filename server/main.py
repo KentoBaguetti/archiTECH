446bec32924be7b75c9295e65f3daded4edf1160
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-11-20 22:38:25 -0800

    get gpt access

--- server/main.py
+++ server/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
 
 app = FastAPI()
+client = OpenAI()
+
+
+class Request(BaseModel):
+    message: str
 
 
 @app.get("/")
 def root():
     return {"msg": "Test"}
+
+
+@app.post("/gpt-response")
+def gpt_response(request_body: Request):
+    response = client.responses.create(model="gpt-5-nano", input=request_body.message)
+    return response.output_text
